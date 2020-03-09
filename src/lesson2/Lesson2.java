33b4201c6ad3d790ba54650baa45513ce64642f0
Kirill Kuznetsov <kuznechik.86@mail.ru>
2020-03-09 20:19:56 +0300

    ДЗ, урок 2 Добавил выпадение исключения в случае квадратного массива с длиной не 4.

--- src/lesson2/Lesson2.java
+++ src/lesson2/Lesson2.java
@@ -3,7 +3,8 @@ package lesson2;
 public class Lesson2 extends InfoExceptions {
     public static final String STR = "10 3 1 2\n2 3 2 2\n5 6 7 1\n300 3 1 0";
     public static String[][] arr;
-    public static int COUNT;
+    public static int count;
+    public static final int COUNT=4;
 
     public Lesson2(String message) {
         super(message);
@@ -12,7 +13,7 @@ public class Lesson2 extends InfoExceptions {
     public static void main(String[] args){
         try {
             createMatrix(STR);
-            int a = strToIntArr(arr,COUNT);
+            int a = strToIntArr(arr, count);
             System.out.println(a);
         } catch (ArrayIndexOutOfBoundsException e){
             infoArrayExceptions();
@@ -27,26 +28,23 @@ public class Lesson2 extends InfoExceptions {
         //Пришлось создать этот метод, потому что если задать длину сразу, при превышении количества строк ошибка не падает.
     }
 
-    private static int strToIntArr(String[][] arr, int COUNT) throws ArrayIndexOutOfBoundsException {
-        int[][] arrInt = new int[COUNT][COUNT];
+    private static int strToIntArr(String[][] arr, int count) throws ArrayIndexOutOfBoundsException {
+        int[][] arrInt = new int[count][count];
         int sum =0;
-        for (int i = 0; i < COUNT; i++) {
-            for (int j = 0; j < COUNT; j++) {
+        for (int i = 0; i < count; i++) {
+            for (int j = 0; j < count; j++) {
                 arrInt[i][j] = Integer.parseInt(String.valueOf(arr[i][j]));
                 sum += arrInt[i][j];
             }
         }
         return sum/2;
     }
-    private static void createMatrix(String str){
-        COUNT  = getLengthRows(str);
-        arr = new String[COUNT][COUNT];
+    private static void createMatrix(String str) throws ArrayIndexOutOfBoundsException{
+        count = getLengthRows(str);
+        if (count !=COUNT) throw new ArrayIndexOutOfBoundsException();
+        arr = new String[count][count];
         String[] arrColumn = str.split("\n");
-        for (int i=0;i<COUNT;i++)
+        for (int i = 0; i< count; i++)
             arr[i] = arrColumn[i].split(" ");
     }
-
-    /*
- 5. * Написать собственные классы исключений для каждого из случаев
-     */
 }
