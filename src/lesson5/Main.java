27753b46f753a2ff80c468afec9d1e8adfe65f0e
Kirill Kuznetsov <kuznechik.86@mail.ru>
2020-03-18 17:38:48 +0300

    ДЗ, урок 5 Все оказалось проще, чем казалось.

--- src/lesson5/Main.java
+++ src/lesson5/Main.java
@@ -5,31 +5,29 @@ public class Main {
     static final int HALF = SIZE / 2;
     static float[] a1 = new float[HALF];
     static float[] a2 = new float[HALF];
-    static float[] arr1 = new float[SIZE];
-    static float[] arr2 = new float[SIZE];
 
     public static void main(String[] args) {
         System.out.println("Start");
         method1();
         method2();
-        System.out.println(arr1[SIZE - 1]);
-        System.out.println(arr2[SIZE - 1]);
     }
 
     public static void method1() {
+        float[] arr = new float[SIZE];
         for (int i = 0; i < SIZE; i++)
-            arr1[i] = 1;
+            arr[i] = 1;
         long a = System.currentTimeMillis();
-        calculate(arr1);
+        calculate(arr);
         System.out.println(System.currentTimeMillis() - a);
     }
 
     public static void method2() {
+        float[] arr = new float[SIZE];
         for (int i = 0; i < SIZE; i++)
-            arr2[i] = 1;
+            arr[i] = 1;
         long a = System.currentTimeMillis();
-        System.arraycopy(arr2, 0, a1, 0, HALF);
-        System.arraycopy(arr2, HALF, a2, 0, HALF);
+        System.arraycopy(arr, 0, a1, 0, HALF);
+        System.arraycopy(arr, HALF, a2, 0, HALF);
         MyThread mt1 = new MyThread(a1);
         MyThread mt2 = new MyThread(a2);
         mt1.start();
@@ -40,8 +38,8 @@ public class Main {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.arraycopy(a1, 0, arr2, 0, HALF);
-        System.arraycopy(a2, 0, arr2, HALF, HALF);
+        System.arraycopy(a1, 0, arr, 0, HALF);
+        System.arraycopy(a2, 0, arr, HALF, HALF);
         System.out.println(System.currentTimeMillis() - a);
     }
 
