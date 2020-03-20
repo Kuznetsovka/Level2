097fe271ceab8e592fb10522deaa298f5c35faad
Kirill Kuznetsov <kuznechik.86@mail.ru>
2020-03-20 19:13:04 +0300

    ДЗ, урок 5. Теперь результат одинаковый.

--- src/lesson5/Main.java
+++ src/lesson5/Main.java
@@ -17,8 +17,9 @@ public class Main {
         for (int i = 0; i < SIZE; i++)
             arr[i] = 1;
         long a = System.currentTimeMillis();
-        calculate(arr);
+        calculate(arr,1);
         System.out.println(System.currentTimeMillis() - a);
+        System.out.println(arr[SIZE-1]);
     }
 
     public static void method2() {
@@ -28,8 +29,8 @@ public class Main {
         long a = System.currentTimeMillis();
         System.arraycopy(arr, 0, a1, 0, HALF);
         System.arraycopy(arr, HALF, a2, 0, HALF);
-        MyThread mt1 = new MyThread(a1);
-        MyThread mt2 = new MyThread(a2);
+        MyThread mt1 = new MyThread(a1,1);
+        MyThread mt2 = new MyThread(a2,2);
         mt1.start();
         mt2.start();
         try {
@@ -41,26 +42,29 @@ public class Main {
         System.arraycopy(a1, 0, arr, 0, HALF);
         System.arraycopy(a2, 0, arr, HALF, HALF);
         System.out.println(System.currentTimeMillis() - a);
+        System.out.println(arr[SIZE-1]);
     }
 
-    public static synchronized void calculate(float[] myArr) {
+    public static synchronized void calculate(float[] myArr, int level) {
         for (int i = 0; i < myArr.length; i++)
-            myArr[i] = (float) (myArr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            myArr[i] = (float) (myArr[i] * Math.sin(0.2f + i *level/ 5) * Math.cos(0.2f + i*level / 5) * Math.cos(0.4f + i*level / 2));
     }
 
     static class MyThread extends Thread {
         float[] myArr;
+        int level;
 
-        public MyThread(float[] myArr) {
+        public MyThread(float[] myArr, int level) {
             super();
             this.myArr = myArr;
+            this.level = level;
         }
 
         @Override
         public void run() {
             if (!isInterrupted()) {
                 try {
-                    calculate(myArr);
+                    calculate(myArr,level);
                     sleep(1);
                 } catch (InterruptedException e) {
                     this.interrupted();
